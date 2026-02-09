ebbb85198d134a40d71ee7dececb3e8710f068a4
Bilal Yasin <byasin@tier2consulting.com>
2026-02-09 16:14:43 +0000

    Remove unused logging configuration and simplify log level settings in host.json

--- Program.cs
+++ Program.cs
@@ -2,7 +2,6 @@ using Microsoft.Azure.Functions.Worker;
 using Microsoft.Azure.Functions.Worker.Builder;
 using Microsoft.Extensions.DependencyInjection;
 using Microsoft.Extensions.Hosting;
-using Microsoft.Extensions.Logging;
 
 var builder = FunctionsApplication.CreateBuilder(args);
 
@@ -11,16 +10,5 @@ builder.ConfigureFunctionsWebApplication();
 builder.Services
     .AddApplicationInsightsTelemetryWorkerService()
     .ConfigureFunctionsApplicationInsights();
-
-builder.Services.Configure<LoggerFilterOptions>(options =>
-{
-    var toRemove = options.Rules.FirstOrDefault(rule => rule.ProviderName 
-        == "Microsoft.Extensions.Logging.ApplicationInsights.ApplicationInsightsLoggerProvider");
-
-    if (toRemove is not null)
-    {
-        options.Rules.Remove(toRemove);
-    }
-});
 
 builder.Build().Run();
