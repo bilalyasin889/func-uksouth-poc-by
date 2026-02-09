2b8b4b183fcd6c4258fca40174d97c51ec6cbd00
Bilal Yasin <byasin@tier2consulting.com>
2026-02-09 15:56:27 +0000

    Refactor logging configuration in host.json and Program.cs to enhance Application Insights integration

--- Program.cs
+++ Program.cs
@@ -2,6 +2,7 @@ using Microsoft.Azure.Functions.Worker;
 using Microsoft.Azure.Functions.Worker.Builder;
 using Microsoft.Extensions.DependencyInjection;
 using Microsoft.Extensions.Hosting;
+using Microsoft.Extensions.Logging;
 
 var builder = FunctionsApplication.CreateBuilder(args);
 
@@ -10,5 +11,16 @@ builder.ConfigureFunctionsWebApplication();
 builder.Services
     .AddApplicationInsightsTelemetryWorkerService()
     .ConfigureFunctionsApplicationInsights();
+
+builder.Services.Configure<LoggerFilterOptions>(options =>
+{
+    var toRemove = options.Rules.FirstOrDefault(rule => rule.ProviderName 
+        == "Microsoft.Extensions.Logging.ApplicationInsights.ApplicationInsightsLoggerProvider");
+
+    if (toRemove is not null)
+    {
+        options.Rules.Remove(toRemove);
+    }
+});
 
 builder.Build().Run();
