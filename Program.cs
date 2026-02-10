1dc5afb0dedcbe046d57ed83d8e382e2530097ea
Bilal Yasin <byasin@tier2consulting.com>
2026-02-10 09:58:04 +0000

    Configure Application Insights logging filter to capture all log levels

--- Program.cs
+++ Program.cs
@@ -12,4 +12,16 @@ builder.Services
     .AddApplicationInsightsTelemetryWorkerService()
     .ConfigureFunctionsApplicationInsights();
 
+builder.Logging.Services.Configure<LoggerFilterOptions>(options =>
+{
+    // The Application Insights SDK adds a default logging filter that instructs ILogger to capture only Warning and more severe logs. Application Insights requires an explicit override.
+    // Log levels can also be configured using appsettings.json. For more information, see https://learn.microsoft.com/azure/azure-monitor/app/worker-service#ilogger-logs
+    LoggerFilterRule? defaultRule = options.Rules.FirstOrDefault(rule => rule.ProviderName
+        == "Microsoft.Extensions.Logging.ApplicationInsights.ApplicationInsightsLoggerProvider");
+    if (defaultRule is not null)
+    {
+        options.Rules.Remove(defaultRule);
+    }
+});
+
 builder.Build().Run();
