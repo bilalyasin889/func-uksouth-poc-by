c5eb725ae450d12831d0cb737b4fc01ed9dc6bbc
Bilal Yasin <byasin@tier2consulting.com>
2026-02-10 10:18:23 +0000

    Add Application Insights logging configuration in Program.cs

--- Program.cs
+++ Program.cs
@@ -2,6 +2,7 @@ using Microsoft.Azure.Functions.Worker;
 using Microsoft.Azure.Functions.Worker.Builder;
 using Microsoft.Extensions.DependencyInjection;
 using Microsoft.Extensions.Hosting;
+using Microsoft.Extensions.Logging;
 
 var builder = FunctionsApplication.CreateBuilder(args);
 
@@ -10,5 +11,8 @@ builder.ConfigureFunctionsWebApplication();
 builder.Services
     .AddApplicationInsightsTelemetryWorkerService()
     .ConfigureFunctionsApplicationInsights();
+
+builder.Logging.AddApplicationInsights();
+builder.Logging.SetMinimumLevel(LogLevel.Information);
 
 builder.Build().Run();
